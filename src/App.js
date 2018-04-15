1cf1da539ebf667996fb1bc35ff393a18c010bea
xgirma <xgirma@gmail.com>
2018-04-15 13:45:18 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>The Beginning - In the beginning God created the heavens and the earth.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
