f80672ba60206a29c06109e4c1c89bb081dd94d4
xgirma <xgirma@gmail.com>
2018-04-15 14:16:28 -0700

    init eslint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Outreach from './Outreach';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// eslint-disable-next-line
+ReactDOM.render(<Outreach />, document.getElementById('root'));
+
 registerServiceWorker();
